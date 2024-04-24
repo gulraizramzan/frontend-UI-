6ae51a26e2a20ec7684652307a186a4f627fcaa6
gulraizramzan <gulraizramzan38@gmail.com>
2024-04-24 13:46:26 +0500

    first coomit

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { FaRegLightbulb } from "react-icons/fa";
+import { MdLightbulb } from "react-icons/md";
 
 const LeftSection = () => {
   return (
     <div className="flex flex-col w-full justify-between md:m-2 space-y-5 md:space-y-5 md:mt-24">
       {/* Icon and Text Section */}
       <div className="flex flex-col items-center">
-        <FaRegLightbulb size={40} color="yellow" />
-        <p className="text-center justify-center text-black mt-2">
+        <div className="bg-blend-multiply flex w-12 h-12 shadow-inner rounded-full justify-center items-center mx-auto">
+          <MdLightbulb size={35} color="yellow" />
+        </div>
+        <p className="text-center leading-5 tracking-tight text-black mt-2">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
           tempora molestias autem quidem,
         </p>
--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -5,11 +5,11 @@ const RightSection = () => {
     <div className="flex flex-col w-full justify-between md:m-2 space-y-5 md:space-y-0 mt-24 md:mt-24">
       {/* Quote Section */}
       <div className="flex flex-col items-center shadow-2xl bg-lightYellow rounded-tl-2xl rounded-br-2xl p-4">
-        <q className="text-center max-w-sm text-black border-2">
+        <q className="text-center leading-5 tracking-tight text-black ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
           delectus recusandae fuga,
         </q>
-        <h6 className="text-center  mt-2">Author</h6>
+        <h6 className="text-justify text-gray-500 mt-2">-Author</h6>
       </div>
 
       {/* Button Section */}
